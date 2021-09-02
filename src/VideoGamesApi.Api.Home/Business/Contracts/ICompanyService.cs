9562d007d056725a1ed0c8c2ad5856dcbf0637d8
Dmitry Karkanitsa <aakarkanica@gmail.com>
2021-09-03 00:36:16 +0300

    Realize services without delete method

--- src/VideoGamesApi.Api.Home/Business/Contracts/ICompanyService.cs
+++ src/VideoGamesApi.Api.Home/Business/Contracts/ICompanyService.cs
@@ -9,13 +9,13 @@ namespace VideoGamesApi.Api.Home.Business.Contracts
     {
         Task<CompanyDto> GetAsync(CompanyQueryModel queryModel);
 
-        Task<CompanyDto> GetListAsync(CompanyQueryModel queryModel);
+        Task<IList<CompanyDto>> GetListAsync(CompanyQueryModel queryModel);
 
-        CompanyDto Modify(CompanyDto dto);
+        Task<CompanyDto> Modify(CompanyDto dto);
 
         Task<CompanyDto> CreateAsync(CompanyDto dto);
 
-        Task<CompanyDto> CreateListAsync(IEnumerable<CompanyDto> dtos);
+        Task CreateListAsync(IEnumerable<CompanyDto> dtos);
 
         Task<CompanyDto> RemoveAsync(CompanyDto dto);
     }
